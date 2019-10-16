ca3252b85b84a8643dc86123f8ae282d5b20224b
Hongbo Deng <denghongbo.oo@gmail.com>
2019-10-16 13:18:02 +0800

    init

---
+++ app/src/main/java/com/ihengtech/facedetect/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ihengtech.facedetect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
