97afb511e4893bf8d759bafcb678368bbab6b28c
denghongbo <denghongbo.oo@gmail.com>
2019-10-18 20:56:38 +0800

    add activity

--- app/src/main/java/com/ihengtech/facedetect/MerchandiseActivity.java
+++ app/src/main/java/com/ihengtech/facedetect/MerchandiseActivity.java
@@ -3,30 +3,23 @@ package com.ihengtech.facedetect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.io.IOException;
-
-public class MainActivity extends AppCompatActivity {
+public class MerchandiseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        setContentView(R.layout.activity_merchandise);
+        Button button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MerchandiseActivity.class);
+                Intent intent = new Intent(MerchandiseActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 }
